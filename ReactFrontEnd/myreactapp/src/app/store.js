a1582e9a9c1bdcd761cf2c3d5762cda752ab77aa
myselfutkarsh <103738117+myselfutkarsh@users.noreply.github.com>
2024-08-07 07:12:48 +0530

    rmoved

--- ReactFrontEnd/myreactapp/src/app/store.js
+++
@@ -1,9 +0,0 @@
-// src/app/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import memberReducer from '../features/memberslice';
-
-export const store = configureStore({
-  reducer: {
-    members: memberReducer,
-  },
-});
--- ReactFrontEnd/myreactapp/src/services/config.js
+++
@@ -1,3 +0,0 @@
-export const config = {
-  serverUrl: 'http://localhost:4000',
-}
