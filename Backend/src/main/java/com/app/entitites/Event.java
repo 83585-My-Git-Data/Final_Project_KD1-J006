50be64a4816ab6fe5bacfd067154d3f2ae9ffd23
KD1-Rutujak-83545 <rutujarkumbhar9@gmail.com>
2024-08-09 11:08:49 +0530

    Events from Admin Side

--- Backend/src/main/java/com/app/entitites/Event.java
+++ Backend/src/main/java/com/app/entitites/Event.java
@@ -5,23 +5,33 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "events")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Event extends BaseEntity
 {
 	@ManyToOne
     @JoinColumn(name = "admin_id", nullable = false)
     private Admin admin;
+	
+    @Enumerated(EnumType.STRING)
+	@Column(name = "eventType", nullable = false)
+	private EventType eventType;
 
     @Column(name = "event_name", nullable = false)
     private String eventName;
---
+++ Backend/src/main/java/com/app/services/EventServiceImpl.java
@@ -0,0 +1,55 @@
+package com.app.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.daos.AdminDao;
+import com.app.daos.EventDao;
+import com.app.dto.EventDTO;
+import com.app.entitites.Admin;
+import com.app.entitites.Event;
+import com.app.entitites.EventType;
+
+@Service
+public class EventServiceImpl implements EventService {
+
+    @Autowired
+    private EventDao eventDao;
+
+    @Autowired
+    private AdminDao adminDao;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+   
+
+    @Override
+    public EventDTO addEvent(EventDTO eventDTO, Long adminId) {  // Ensure the return type matches
+        Optional<Admin> adminOptional = adminDao.findById(adminId);
+        if (!adminOptional.isPresent()) {
+            throw new RuntimeException("Admin not found with id: " + adminId);
+        }
+        Admin admin = adminOptional.get();
+        Event event = modelMapper.map(eventDTO, Event.class);
+        event.setAdmin(admin);
+        Event savedEvent = eventDao.save(event);
+        return mapToEventDTO(savedEvent);
+    }
+
+    @Override
+    public List<EventDTO> getEventsByType(EventType eventType) {
+        List<Event> events = eventDao.findByEventType(eventType);
+        return events.stream().map(this::mapToEventDTO).collect(Collectors.toList());
+    }
+
+   
+    private EventDTO mapToEventDTO(Event event) {
+        return modelMapper.map(event, EventDTO.class);
+    }
+}
